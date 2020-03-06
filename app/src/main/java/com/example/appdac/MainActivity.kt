cd41ed3ca4cd8a1e3de1b9e50149e6f7af860629
elisales <elisalesmunizlima@gmail.com>
2020-03-05 22:03:38 -0300

    segunda tela

--- app/src/main/java/com/example/appdac/MainActivity.kt
+++ app/src/main/java/com/example/appdac/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.appdac
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        botao_Login.setOnClickListener{
+            proximatela()
+        }
+
+
     }
+
+    private fun proximatela(){
+        val intent = Intent(this, menualuno000::class.java)
+        startActivity(intent)
+    }
+
 }
